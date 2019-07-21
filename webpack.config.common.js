c0c2231bd710180537bf333b39c4fe752c9d1006
Danny Murphy <danny.murphy@gmail.com>
2019-07-21 10:44:40 +0100

    Removed font file loader from webpack

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -76,18 +76,6 @@ const webpackConfig = {
               ],
             },
           },
-          {
-            test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-            use: [
-              {
-                loader: 'file-loader',
-                options: {
-                  name: '[name].[ext]',
-                  outputPath: 'fonts',
-                },
-              },
-            ],
-          },
         ],
       },
     ],
