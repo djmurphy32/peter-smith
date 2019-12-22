cef1e6a5f1849a88e8173b230e7da515c9e0e120
Danny Murphy <danjmurphy92@gmail.com>
2019-12-22 13:16:14 +0000

    Dynamically import all images from portfolio directory (#22)
    * Dynamically import all images from portfolio directory
    
    * Udo changes to tsconfig

---
+++ src/web-client/utils/importAll.ts
@@ -0,0 +1,9 @@
+function importAll(context: __WebpackModuleApi.RequireContext) {
+  const images: { [index: string]: any } = {}
+  context.keys().map((item) => {
+    images[item.replace('./', '')] = context(item)
+  })
+  return images
+}
+
+export default importAll
