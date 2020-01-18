8290e36ff1e445253c12e49ff3fa6c403576961e
Danny <danjmurphy92@gmail.com>
2020-01-18 10:12:10 +0000

    Added tests for unbind

--- src/web-client/directives/__tests__/externalClick.spec.ts
+++ src/web-client/directives/__tests__/externalClick.spec.ts
@@ -2,21 +2,19 @@ import externalClick from '../externalClick'
 import { VNodeDirective, VNode } from 'vue/types/umd'
 
 describe('Directive - externlClick', () => {
-  let docEventListenerSpy: jest.SpyInstance
   let el: HTMLElement
   let binding: VNodeDirective
   let vnode: VNode
-
-  beforeEach(() => {
-    docEventListenerSpy = jest.spyOn(document, 'addEventListener')
-  })
 
   afterEach(() => {
     jest.clearAllMocks()
   })
 
   describe('bind', () => {
+    let docEventListenerSpy: jest.SpyInstance
+
     beforeEach(() => {
+      docEventListenerSpy = jest.spyOn(document, 'addEventListener')
       el = document.createElement('div')
       binding = {
         name: 'binding',
@@ -28,6 +26,28 @@ describe('Directive - externlClick', () => {
 
     it('THEN binds to document', () => {
       expect(docEventListenerSpy).toBeCalledTimes(1)
+    })
+
+    describe('unbind', () => {
+      let removeEventListenerSpy: jest.SpyInstance
+      const elExternalClick = { foo: 'bar' }
+
+      beforeEach(() => {
+        ;(el as any).onClickExternal = elExternalClick
+        removeEventListenerSpy = jest.spyOn(document, 'removeEventListener')
+
+        // @ts-ignore
+        externalClick.unbind(el)
+      })
+
+      it('THEN removes event listener from document', () => {
+        expect(removeEventListenerSpy).toBeCalledTimes(1)
+        expect(removeEventListenerSpy).toBeCalledWith('click', elExternalClick)
+      })
+
+      it('THEN deletes the directive form the element', () => {
+        expect((el as any).onClickExternal).toBeUndefined()
+      })
     })
   })
 })
