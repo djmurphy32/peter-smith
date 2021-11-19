19c7092fa30d44cb1f2d458c6b70b369b32d4d97
Danny Murphy <danjmurphy92@gmail.com>
2021-11-19 16:58:28 +0000

    Added placeholder images for portfolio

--- src/web-client/router/pages/HomePage.vue
+++ src/web-client/router/pages/HomePage.vue
@@ -2,33 +2,19 @@
   <div class="homepage">
     <LittleHillsPortfolio />
     <BrightAndYoungPortfolio />
-    <div class="homepage-image-container">
-      <ImageCarousel image-class="homepage-image" :images="images" :full-width="1000" :lazy-width="200" />
-    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import ImageCarousel from '@/components/ImageCarousel.vue'
 import LittleHillsPortfolio from '@/components/portfolio/LittleHillsPortfolio.vue'
 import BrightAndYoungPortfolio from '@/components/portfolio/BrightAndYoungPortfolio.vue'
-import homeContext from '@/utils/webpackContexts/home'
-import importAll from '@/utils/importAll'
-
-const requiredImages = importAll(homeContext)
 
 export default Vue.extend({
   name: 'HomePage',
   components: {
-    ImageCarousel,
     LittleHillsPortfolio,
     BrightAndYoungPortfolio,
-  },
-  data() {
-    return {
-      images: Object.keys(requiredImages).map((image, i) => ({ src: requiredImages[image], alt: `image_${i}` })),
-    }
   },
 })
 </script>
---
+++ src/web-client/utils/webpackContexts/littleHills.ts
@@ -0,0 +1,1 @@
+export default require.context('@/assets/images/portfolio/littleHills', false, /\.jpg$/)
