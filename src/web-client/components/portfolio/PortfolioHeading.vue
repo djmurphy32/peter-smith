0101e4b9668842426eb416d69b2fe50001e248ef
Danny Murphy <danjmurphy92@gmail.com>
2021-11-19 16:26:31 +0000

    Added colophon and bits

---
+++ src/web-client/components/portfolio/PortfolioHeading.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="portfolio-heading">
+    <div class="portfolio-heading__title">
+      <span>{{ title }}</span>
+    </div>
+
+    <div class="portfolio-heading__body">
+      <p v-for="(text, ix) in body" :key="ix">{{ text }}</p>
+    </div>
+    <div class="portfolio-heading__footer">
+      <span class="portfolio-heading__footer__line" v-for="(text, ix) in footer" :key="ix">{{ text }}</span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue'
+
+export default Vue.extend({
+  name: 'PortfolioHeading',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: Array as PropType<String[]>,
+      required: true,
+    },
+    footer: {
+      type: Array as PropType<String[]>,
+      required: true,
+    },
+  },
+})
+</script>
+
+<style lang="scss">
+.portfolio-heading {
+  margin-bottom: 20px;
+
+  &__title {
+    font-weight: 600;
+  }
+
+  &__footer {
+    &__line {
+      display: block;
+    }
+  }
+}
+</style>
--- src/web-client/router/pages/HomePage.vue
+++ src/web-client/router/pages/HomePage.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="homepage">
+    <LittleHillsPortfolio />
+    <BrightAndYoungPortfolio />
     <div class="homepage-image-container">
       <ImageCarousel imageClass="homepage-image" :images="images" :fullWidth="1000" :lazyWidth="200" />
     </div>
@@ -10,6 +12,8 @@
 import Vue from 'vue'
 import ContactDetails from '@/components/ContactDetails.vue'
 import ImageCarousel from '@/components/ImageCarousel.vue'
+import LittleHillsPortfolio from '@/components/portfolio/LittleHillsPortfolio.vue'
+import BrightAndYoungPortfolio from '@/components/portfolio/BrightAndYoungPortfolio.vue'
 import homeContext from '@/utils/webpackContexts/home'
 import importAll from '@/utils/importAll'
 
@@ -20,6 +24,8 @@ export default Vue.extend({
   components: {
     ContactDetails,
     ImageCarousel,
+    LittleHillsPortfolio,
+    BrightAndYoungPortfolio,
   },
   data() {
     return {
