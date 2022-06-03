eccfe1e0103223a59b2fceaade68e5a5f9da73be
Danny Murphy <danjmurphy92@gmail.com>
2022-06-03 09:16:54 +0100

    Setting esbuild target

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite'
 import { createVuePlugin } from 'vite-plugin-vue2'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [createVuePlugin(/* options */)],
+  plugins: [createVuePlugin()],
+  esbuild: {
+    target: 'es2020',
+  },
   css: {
     preprocessorOptions: {
       scss: {
-        // example : additionalData: `@import "./src/design/styles/variables";`
-        // dont need include file extend .scss
         additionalData: `@import "./src/web-client/styles/functions/_layout";@import "./src/web-client/styles/variables/_all"; `,
       },
     },
