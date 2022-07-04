6eaf875ee13c5bc33ef2afd396dbef74f8fd1956
Danny Murphy <danjmurphy92@gmail.com>
2022-07-04 12:25:19 +0100

    Updated Kalvin Phillips club (#104)

--- src/web-client/components/portfolio/HommePortfolio.vue
+++ src/web-client/components/portfolio/HommePortfolio.vue
@@ -10,7 +10,7 @@ const importedImages = import.meta.globEager('../../assets/images/portfolio/homm
 const images = ref(importedImages)
 const title = ref('Arena HOMME+ 57')
 const body = ref([
-  'LOEWE special with England and Leeds United Midfielder Kalvin Phillips. Photographed in his hometown of Cleckheaton.',
+  'LOEWE special with England and Man City Midfielder Kalvin Phillips. Photographed in his hometown of Cleckheaton.',
 ])
 const footer = ref(['Words - James Brown', 'Styling - Charlotte Rubenstein', 'Grooming - James Duggan'])
 </script>
