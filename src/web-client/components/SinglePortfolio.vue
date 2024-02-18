b78c314e9f01576213682061379373400dc3be36
Danny Murphy <danjmurphy92@gmail.com>
2024-02-18 10:15:35 +0000

    Moved all portfolio setup into a single component (#118)
    * Moved to a single portfolio component
    
    * Fixed file ref
    
    * Deleted logo.png
    
    * Fixed asset reference
    
    * Added missing portfolio
    
    * fixed path
    
    * Deleting old portfolio components

---
+++ src/web-client/components/SinglePortfolio.vue
@@ -0,0 +1,125 @@
+<template>
+  <Portfolio
+    v-for="config in configs"
+    :key="config.title"
+    :body="config.body"
+    :imported-images="config.images"
+    :title="config.title"
+    :footer="config.footer"
+    :video-link="config.videoLink"
+  />
+</template>
+
+<script setup lang="ts">
+import Portfolio from './ImagePortfolio.vue';
+import { GlobEagerImport } from '@/typings/globImport';
+
+type PortfolioConfig = {
+  title: string;
+  body: string[];
+  images: GlobEagerImport;
+  footer?: string[];
+  videoLink?: string;
+};
+
+const configs: PortfolioConfig[] = [
+  {
+    title: 'Arena Homme+ 60',
+    body: ['The Collections'],
+    images: import.meta.glob('../assets/images/portfolio/AH60_TheCollections/*.jpg', {
+      eager: true,
+    }) as GlobEagerImport,
+  },
+  {
+    title: 'Arena Homme+ 60',
+    body: ['An Cat Dubh', 'with Natasha Arnold'],
+    images: import.meta.glob('../assets/images/portfolio/AH60_AnCatDubh/*.jpg', {
+      eager: true,
+    }) as GlobEagerImport,
+  },
+  {
+    title: "'W'Happen?",
+    body: ['Cult of Mowalola'],
+    videoLink: 'https://player.vimeo.com/video/914040307?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479',
+    images: import.meta.glob('../assets/images/portfolio/Pop_Mowalola/*.jpg', {
+      eager: true,
+    }) as GlobEagerImport,
+  },
+  {
+    title: 'Arena Homme+ 59',
+    body: ['The Collections'],
+    images: import.meta.glob('../assets/images/portfolio/AH59_TheCollections/*.jpg', {
+      eager: true,
+    }) as GlobEagerImport,
+  },
+  {
+    title: 'Arena Homme+ 58',
+    body: ['Wish We Could All Be (California Boys)'],
+    images: import.meta.glob('../assets/images/portfolio/AH58_CaliforniaBoys/*.jpg', {
+      eager: true,
+    }) as GlobEagerImport,
+  },
+  {
+    title: 'Arena Homme+ 58',
+    body: ['The Collections'],
+    images: import.meta.glob('../assets/images/portfolio/AH58_TheCollections/*.jpg', {
+      eager: true,
+    }) as GlobEagerImport,
+  },
+  {
+    title: 'POP Magazine 48',
+    body: ['Stella Rose Gahan'],
+    images: import.meta.glob('../assets/images/portfolio/Pop_StellaRose/*.jpg', {
+      eager: true,
+    }) as GlobEagerImport,
+  },
+  {
+    title: 'i-D Magazine Spring 23',
+    body: ['Three'],
+    images: import.meta.glob('../assets/images/portfolio/ID_Spring23/*.jpg', {
+      eager: true,
+    }) as GlobEagerImport,
+  },
+  {
+    title: 'i-D Magazine Fall 22',
+    body: ['Jockstrap'],
+    videoLink: 'https://player.vimeo.com/video/913733822?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479',
+    images: import.meta.glob('../assets/images/portfolio/ID_Fall22/*.jpg', {
+      eager: true,
+    }) as GlobEagerImport,
+  },
+  {
+    title: 'Arena HOMME+ 57',
+    body: [
+      'LOEWE special with England and Man City Midfielder Kalvin Phillips. Photographed in his hometown of Cleckheaton.',
+    ],
+    footer: ['Words - James Brown', 'Styling - Charlotte Rubenstein', 'Grooming - James Duggan'],
+    images: import.meta.glob('../assets/images/portfolio/AH57_KalvinPhillips/*.jpg', {
+      eager: true,
+    }) as GlobEagerImport,
+  },
+  {
+    title: 'Replica Magazine',
+    body: ['Men’s fashion story for Replica’s tenth issue - Spring 22'],
+    footer: [
+      'Styling - Louis Prier Tisdall',
+      'Featuring Matej, Donte, Brennan and Eliot',
+      'c/o Anita Bitton - Establishment',
+    ],
+    images: import.meta.glob('../assets/images/portfolio/Replica_Spring22/*.jpg', {
+      eager: true,
+    }) as GlobEagerImport,
+  },
+  {
+    title: 'Little Hills',
+    body: [
+      'A portfolio of pictures made across the towns and villages of the North East of Ireland.',
+      "Pictures of the landscapes, the events, and the people that influence today's youth.",
+    ],
+    footer: ['Edition of 200', 'Loose Leaf', '42 Pages'],
+    images: import.meta.glob('../assets/images/portfolio/littleHills/*.jpg', {
+      eager: true,
+    }) as GlobEagerImport,
+  },
+];
+</script>
--- src/web-client/router/pages/HomePage.vue
+++ src/web-client/router/pages/HomePage.vue
@@ -1,33 +1,11 @@
 <template>
   <div class="homepage">
-    <AH60TheCollections />
-    <AH60AnCatDubh />
-    <PopMowalola />
-    <AH59TheCollections />
-    <AH58CaliforniaBoys />
-    <AH58TheCollections />
-    <PopStellaRose />
-    <IDSpring23 />
-    <IDFall22 />
-    <AH57KalvinPhillips />
-    <ReplicaSpring22 />
-    <LittleHills />
+    <SinglePortfolio />
   </div>
 </template>
 
 <script setup lang="ts">
-import AH60TheCollections from '@/components/portfolio/AH60TheCollections.vue';
-import IDFall22 from '@/components/portfolio/IDFall22.vue';
-import IDSpring23 from '@/components/portfolio/IDSpring23.vue';
-import PopStellaRose from '@/components/portfolio/PopStellaRose.vue';
-import AH58TheCollections from '@/components/portfolio/AH58TheCollections.vue';
-import AH58CaliforniaBoys from '@/components/portfolio/AH58CaliforniaBoys.vue';
-import AH59TheCollections from '@/components/portfolio/AH59TheCollections.vue';
-import PopMowalola from '@/components/portfolio/PopMowalola.vue';
-import AH60AnCatDubh from '@/components/portfolio/AH60AnCatDubh.vue';
-import AH57KalvinPhillips from '@/components/portfolio/AH57KalvinPhillips.vue';
-import ReplicaSpring22 from '@/components/portfolio/ReplicaSpring22.vue';
-import LittleHills from '@/components/portfolio/LittleHills.vue';
+import SinglePortfolio from '@/components/SinglePortfolio.vue';
 </script>
 
 <style lang="scss">
