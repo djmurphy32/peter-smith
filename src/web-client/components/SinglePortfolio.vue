cbbfdb2b5cb84f81639cc1449f0a6f9ffdc2c2f5
Danny Murphy <danjmurphy92@gmail.com>
2024-02-18 10:04:40 +0000

    Fixed asset reference

--- src/web-client/components/SinglePortfolio.vue
+++ src/web-client/components/SinglePortfolio.vue
@@ -26,14 +26,14 @@ const configs: PortfolioConfig[] = [
   {
     title: 'Arena Homme+ 60',
     body: ['The Collections'],
-    images: import.meta.glob('../../assets/images/portfolio/AH60_TheCollections/*.jpg', {
+    images: import.meta.glob('../assets/images/portfolio/AH60_TheCollections/*.jpg', {
       eager: true,
     }) as GlobEagerImport,
   },
   {
     title: 'Arena Homme+ 60',
     body: ['An Cat Dubh', 'with Natasha Arnold'],
-    images: import.meta.glob('../../assets/images/portfolio/AH60_AnCatDubh/*.jpg', {
+    images: import.meta.glob('../assets/images/portfolio/AH60_AnCatDubh/*.jpg', {
       eager: true,
     }) as GlobEagerImport,
   },
@@ -41,35 +41,35 @@ const configs: PortfolioConfig[] = [
     title: "'W'Happen?",
     body: ['Cult of Mowalola'],
     videoLink: 'https://player.vimeo.com/video/914040307?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479',
-    images: import.meta.glob('../../assets/images/portfolio/Pop_Mowalola/*.jpg', {
+    images: import.meta.glob('../assets/images/portfolio/Pop_Mowalola/*.jpg', {
       eager: true,
     }) as GlobEagerImport,
   },
   {
     title: 'Arena Homme+ 58',
     body: ['Wish We Could All Be (California Boys)'],
-    images: import.meta.glob('../../assets/images/portfolio/AH58_CaliforniaBoys/*.jpg', {
+    images: import.meta.glob('../assets/images/portfolio/AH58_CaliforniaBoys/*.jpg', {
       eager: true,
     }) as GlobEagerImport,
   },
   {
     title: 'Arena Homme+ 58',
     body: ['The Collections'],
-    images: import.meta.glob('../../assets/images/portfolio/AH58_TheCollections/*.jpg', {
+    images: import.meta.glob('../assets/images/portfolio/AH58_TheCollections/*.jpg', {
       eager: true,
     }) as GlobEagerImport,
   },
   {
     title: 'POP Magazine 48',
     body: ['Stella Rose Gahan'],
-    images: import.meta.glob('../../assets/images/portfolio/Pop_StellaRose/*.jpg', {
+    images: import.meta.glob('../assets/images/portfolio/Pop_StellaRose/*.jpg', {
       eager: true,
     }) as GlobEagerImport,
   },
   {
     title: 'i-D Magazine Spring 23',
     body: ['Three'],
-    images: import.meta.glob('../../assets/images/portfolio/ID_Spring23/*.jpg', {
+    images: import.meta.glob('../assets/images/portfolio/ID_Spring23/*.jpg', {
       eager: true,
     }) as GlobEagerImport,
   },
@@ -77,7 +77,7 @@ const configs: PortfolioConfig[] = [
     title: 'i-D Magazine Fall 22',
     body: ['Jockstrap'],
     videoLink: 'https://player.vimeo.com/video/913733822?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479',
-    images: import.meta.glob('../../assets/images/portfolio/ID_Fall22/*.jpg', {
+    images: import.meta.glob('../assets/images/portfolio/ID_Fall22/*.jpg', {
       eager: true,
     }) as GlobEagerImport,
   },
@@ -87,7 +87,7 @@ const configs: PortfolioConfig[] = [
       'LOEWE special with England and Man City Midfielder Kalvin Phillips. Photographed in his hometown of Cleckheaton.',
     ],
     footer: ['Words - James Brown', 'Styling - Charlotte Rubenstein', 'Grooming - James Duggan'],
-    images: import.meta.glob('../../assets/images/portfolio/AH57_KalvinPhillips/*.jpg', {
+    images: import.meta.glob('../assets/images/portfolio/AH57_KalvinPhillips/*.jpg', {
       eager: true,
     }) as GlobEagerImport,
   },
@@ -99,7 +99,7 @@ const configs: PortfolioConfig[] = [
       'Featuring Matej, Donte, Brennan and Eliot',
       'c/o Anita Bitton - Establishment',
     ],
-    images: import.meta.glob('../../assets/images/portfolio/Replica_Spring22/*.jpg', {
+    images: import.meta.glob('../assets/images/portfolio/Replica_Spring22/*.jpg', {
       eager: true,
     }) as GlobEagerImport,
   },
@@ -110,7 +110,7 @@ const configs: PortfolioConfig[] = [
       "Pictures of the landscapes, the events, and the people that influence today's youth.",
     ],
     footer: ['Edition of 200', 'Loose Leaf', '42 Pages'],
-    images: import.meta.glob('../../assets/images/portfolio/littleHills/*.jpg', {
+    images: import.meta.glob('../assets/images/portfolio/littleHills/*.jpg', {
       eager: true,
     }) as GlobEagerImport,
   },
