c1c37bc15fd750e7fdd0b2f0dd93ed4f01723090
Danny Murphy <danjmurphy92@gmail.com>
2024-02-18 10:00:51 +0000

    Fixed file ref

--- src/web-client/router/pages/HomePage.vue
+++ src/web-client/router/pages/HomePage.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script setup lang="ts">
-import SinglePortfolio from '@/components/portfolio/SinglePortfolio.vue';
+import SinglePortfolio from '@/components/SinglePortfolio.vue';
 </script>
 
 <style lang="scss">
