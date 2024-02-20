59e83355425781e9308e702d4353bf637b9d3bb3
Danny Murphy <danjmurphy92@gmail.com>
2024-02-20 18:40:53 +0000

    deleting little hills

--- src/web-client/components/PortfoliosWrapper.vue
+++ src/web-client/components/PortfoliosWrapper.vue
@@ -110,16 +110,16 @@ const configs: PortfolioConfig[] = [
   //     eager: true,
   //   }) as GlobEagerImport,
   // },
-  {
-    title: 'Little Hills',
-    body: [
-      'A portfolio of pictures made across the towns and villages of the North East of Ireland.',
-      "Pictures of the landscapes, the events, and the people that influence today's youth.",
-    ],
-    footer: ['Edition of 200', 'Loose Leaf', '42 Pages'],
-    images: import.meta.glob('../assets/images/portfolio/littleHills/*.jpg', {
-      eager: true,
-    }) as GlobEagerImport,
-  },
+  // {
+  //   title: 'Little Hills',
+  //   body: [
+  //     'A portfolio of pictures made across the towns and villages of the North East of Ireland.',
+  //     "Pictures of the landscapes, the events, and the people that influence today's youth.",
+  //   ],
+  //   footer: ['Edition of 200', 'Loose Leaf', '42 Pages'],
+  //   images: import.meta.glob('../assets/images/portfolio/littleHills/*.jpg', {
+  //     eager: true,
+  //   }) as GlobEagerImport,
+  // },
 ];
 </script>
