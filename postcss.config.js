5cca7add0608e0443c45f56c15a0093c7aa0e75c
Danny Murphy <dmurphy@forbesadvisor.com>
2024-11-01 08:45:45 +0000

    cleanup

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,6 @@
 import autoprefixer from "autoprefixer";
 import tailwind from "tailwindcss";
-import cssMqPacker from "css-mqpacker";
-import cssMqPackerSort from "./postcss/cssMqPackerSort.js";
 
 export default {
-  plugins: [autoprefixer, tailwind, cssMqPacker({ sort: cssMqPackerSort })],
+  plugins: [autoprefixer, tailwind],
 };
