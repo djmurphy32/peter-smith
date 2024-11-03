4041288bd20227c757d8adc7df29669e1a09be41
Danny Murphy <dmurphy@forbesadvisor.com>
2024-11-03 11:36:46 +0000

    fixing import

--- src/web-client/components/v2/Portfolio.vue
+++ src/web-client/components/v2/Portfolio.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 import LazyImage from "@/components/LazyImage.vue";
+import Img from "@/assets/images/portfolio/AH60_TheCollections/Peter_joseph_smith_peterjosephsmith_website_website6560.jpg";
 </script>
 
 <template>
   <LazyImage
-    src="/src/web-client/assets/images/portfolio/AH60_TheCollections/Peter_joseph_smith_peterjosephsmith_website_website6560.jpg"
+    :src="Img"
     alt="alt"
     :full-width="1200"
     :lazy-width="200"
-    :css-width="400"
+    :css-width="600"
   />
 </template>
