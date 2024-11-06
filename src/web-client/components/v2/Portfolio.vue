3d07a37e7f088c42ac1a5cf5aba5b3091db04e2d
Danny Murphy <dmurphy@forbesadvisor.com>
2024-11-06 06:50:03 +0000

    Adding carousel

--- src/web-client/components/v2/Portfolio.vue
+++ src/web-client/components/v2/Portfolio.vue
@@ -1,14 +1,51 @@
 <script setup lang="ts">
 import LazyImage from "@/components/LazyImage.vue";
-import Img from "@/assets/images/portfolio/AH60_TheCollections/Peter_joseph_smith_peterjosephsmith_website_website6560.jpg";
+import { GlobEagerImport } from "@/typings/globImport";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/carousel";
+import { computed } from "vue";
+
+const importedImgs = import.meta.glob(
+  "../../assets/images/portfolio/AH60_TheCollections/*.jpg",
+  {
+    eager: true,
+  }
+) as GlobEagerImport;
+
+const images = computed((): { src: string; alt: string }[] => {
+  const paths = Object.values(importedImgs).map((module) => module.default);
+
+  return paths.map((image, i) => ({ src: image, alt: `image_${i}` }));
+});
 </script>
 
 <template>
-  <LazyImage
-    :src="Img"
-    alt="alt"
-    :full-width="1200"
-    :lazy-width="200"
-    :css-width="600"
-  />
+  <Carousel v-slot="{ canScrollNext }" class="relative w-full max-w-xs">
+    <CarouselContent>
+      <CarouselItem v-for="img in images" :key="img.src">
+        <div class="p-1">
+          <Card>
+            <CardContent
+              class="flex aspect-square items-center justify-center p-6"
+            >
+              <LazyImage
+                :src="img.src"
+                :alt="img.alt"
+                :full-width="1200"
+                :lazy-width="200"
+                :css-width="600"
+              />
+            </CardContent>
+          </Card>
+        </div>
+      </CarouselItem>
+    </CarouselContent>
+    <CarouselPrevious />
+    <CarouselNext v-if="canScrollNext" />
+  </Carousel>
 </template>
