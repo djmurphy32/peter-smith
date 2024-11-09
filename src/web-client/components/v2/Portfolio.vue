967aca32fb5271e1358d9702d845e30404305756
Danny Murphy <danjmurphy92@gmail.com>
2024-11-09 09:56:51 +0000

    Adding v2 Carousel (#152)
    * npm i
    
    * adding shaddcdn
    
    * Adding carousel
    
    * lazy loading images for carousel
    
    * Adding overlays for scrolling
    
    * Adding nudge animation
    
    ---------
    
    Co-authored-by: Danny Murphy <dmurphy@forbesadvisor.com>
    Co-authored-by: Danny Murphy <danny@Mac.Home>

--- src/web-client/components/v2/Portfolio.vue
+++ src/web-client/components/v2/Portfolio.vue
@@ -1,14 +1,127 @@
 <script setup lang="ts">
-import LazyImage from "@/components/LazyImage.vue";
-import Img from "@/assets/images/portfolio/AH60_TheCollections/Peter_joseph_smith_peterjosephsmith_website_website6560.jpg";
+import { GlobEagerImport } from "@/typings/globImport";
+import { Carousel, CarouselContent, CarouselItem } from "@/components/carousel";
+import type { CarouselApi } from "@/components/carousel";
+import { computed, ref, watch } from "vue";
+import { watchOnce } from "@vueuse/core";
+
+const importedImgs = import.meta.glob(
+  "../../assets/images/portfolio/AH60_TheCollections/*.jpg",
+  {
+    eager: true,
+  }
+) as GlobEagerImport;
+
+const imagesSrcs = computed((): string[] =>
+  Object.values(importedImgs).map((module) => module.default)
+);
+
+const api = ref<CarouselApi>();
+function setApi(val: CarouselApi) {
+  api.value = val;
+}
+
+const currentCarouselItem = ref(0);
+const hasInteractedWithCarousel = ref(false);
+watchOnce(api, (api) => {
+  if (!api) {
+    return;
+  }
+  currentCarouselItem.value = api.selectedScrollSnap();
+
+  api.on("select", () => {
+    hasInteractedWithCarousel.value = true;
+    currentCarouselItem.value = api.selectedScrollSnap();
+  });
+});
+const viewedCarouselItems = ref<number[]>([
+  currentCarouselItem.value,
+  currentCarouselItem.value + 1,
+  imagesSrcs.value.length - 1,
+]);
+
+watch(currentCarouselItem, (val) => {
+  if (viewedCarouselItems.value.length === imagesSrcs.value.length) {
+    return;
+  }
+
+  if (!viewedCarouselItems.value.includes(val)) {
+    viewedCarouselItems.value.push(val);
+  }
+
+  const nextItem = val + 1;
+  if (!viewedCarouselItems.value.includes(nextItem)) {
+    viewedCarouselItems.value.push(nextItem);
+  }
+
+  const prevItem = val - 1;
+  if (!viewedCarouselItems.value.includes(prevItem)) {
+    viewedCarouselItems.value.push(prevItem);
+  }
+});
+
+const images = computed<{ src: string; key: string }[]>(() => {
+  return imagesSrcs.value.map((origSrc, ix) => {
+    const isViewed = viewedCarouselItems.value.includes(ix);
+    if (!isViewed) {
+      return { src: "", key: origSrc };
+    }
+    return { src: `/.netlify/images?url=${origSrc}&w=1200`, key: origSrc };
+  });
+});
 </script>
 
 <template>
-  <LazyImage
-    :src="Img"
-    alt="alt"
-    :full-width="1200"
-    :lazy-width="200"
-    :css-width="600"
-  />
+  <Carousel
+    v-slot="{ canScrollPrev, canScrollNext }"
+    @init-api="setApi"
+    :opts="{
+      loop: true,
+    }"
+    class="relative w-full max-w-xl"
+  >
+    <CarouselContent>
+      <CarouselItem
+        v-for="img in images"
+        :key="img.key"
+        class="pl-1"
+        :class="{
+          'animate-nudge': !hasInteractedWithCarousel,
+        }"
+      >
+        <div class="relative">
+          <div
+            v-if="canScrollPrev"
+            @click="api?.scrollPrev()"
+            class="absolute top-0 left-0 w-1/2 h-full bg-black bg-opacity-50 z-10"
+          ></div>
+          <div
+            v-if="canScrollNext"
+            @click="api?.scrollNext()"
+            class="absolute top-0 right-0 w-1/2 h-full bg-black bg-opacity-50 z-10"
+          ></div>
+          <img v-if="img.src" :src="img.src" />
+        </div>
+      </CarouselItem>
+    </CarouselContent>
+  </Carousel>
 </template>
+
+<style scoped>
+@keyframes nudge {
+  0%,
+  30%,
+  70%,
+  100% {
+    transform: translateX(0);
+  }
+  50% {
+    transform: translateX(-10px);
+  }
+}
+
+.animate-nudge {
+  animation: nudge 5s ease-in-out 3;
+  animation-delay: 2s;
+}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,11 @@
+const animate = require("tailwindcss-animate")
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
   content: ["./src/web-client/**/*.{ts,tsx,vue}"],
   theme: {
-    // fontFamily: {
-    //   sans: ["var(--font-family)", "sans-serif"],
-    // },
     colors: {
-      black: "#000",
-      white: "#fff",
       neutral: "#e8e9e5",
     },
     container: {
@@ -20,8 +17,44 @@ module.exports = {
     },
     extend: {
       colors: {
-        background: "var(--background)",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+      },
+      borderRadius: {
+        xl: "calc(var(--radius) + 4px)",
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-};
+  plugins: [animate],
+}
