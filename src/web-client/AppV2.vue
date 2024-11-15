b7759c56b73b8d1e7abedd5ec9de3d8abba0807b
Danny <danjmurphy92@gmail.com>
2024-11-15 17:55:57 +0000

    last css changes

--- src/web-client/AppV2.vue
+++ src/web-client/AppV2.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import Sidebar from "@/components/v2/Sidebar.vue";
+import Name from "@/components/v2/Name.vue";
 import SiteFooter from "@/components/v2/SiteFooter.vue";
 </script>
 
@@ -8,8 +8,11 @@ import SiteFooter from "@/components/v2/SiteFooter.vue";
     <div
       class="m-4 flex flex-col md:m-0 md:grid md:grid-cols-[200px,auto,200px] gap-4"
     >
-      <Sidebar />
-
+      <div class="flex justify-center">
+        <div class="md:fixed md:top-1/2 md:transform md:-translate-y-1/2">
+          <Name />
+        </div>
+      </div>
       <div class="flex-grow grid grid-rows-[1fr,auto] md:pt-16">
         <div class="flex justify-center items-center">
           <router-view />
--- src/web-client/components/v2/Sidebar.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts">
-import Name from "@/components/v2/Name.vue";
-</script>
-<template>
-  <div class="flex justify-center md:items-center">
-    <Name />
-  </div>
-</template>
