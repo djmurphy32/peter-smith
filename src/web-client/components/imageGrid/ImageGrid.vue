7874a95d704fab13188d5d436ce86804df229574
Danny <danjmurphy92@gmail.com>
2024-11-24 07:25:22 +0000

    changing order of obervers

--- src/web-client/components/imageGrid/ImageGrid.vue
+++ src/web-client/components/imageGrid/ImageGrid.vue
@@ -1,37 +1,18 @@
 <script setup lang="ts">
 import { useIntersectionObserver } from "@vueuse/core";
-import { computed, onMounted, ref, useTemplateRef } from "vue";
+import { computed, ref, useTemplateRef } from "vue";
 
 interface Props {
   images: { src: string; lowResSrc: string; key: string }[];
   selectedItem?: number;
 }
 
-const { images, selectedItem } = defineProps<Props>();
+const { images } = defineProps<Props>();
 const imageRefs = useTemplateRef<Element[]>("image");
-const lowResItems = ref<number[]>([]);
 
 const emit = defineEmits<{
   (e: "update:selectItem", value: number): void;
 }>();
-
-useIntersectionObserver(
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  imageRefs as any,
-  (entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        const index = imageRefs.value?.indexOf(entry.target);
-        if (index) {
-          if (!lowResItems.value.includes(index)) {
-            lowResItems.value.push(index);
-          }
-        }
-      }
-    });
-  },
-  { rootMargin: "200px 0px" },
-);
 
 const fullItems = ref<number[]>([]);
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -47,6 +28,25 @@ useIntersectionObserver(imageRefs as any, (entries) => {
     }
   });
 });
+
+const lowResItems = ref<number[]>([]);
+useIntersectionObserver(
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  imageRefs as any,
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const index = imageRefs.value?.indexOf(entry.target);
+        if (index) {
+          if (!lowResItems.value.includes(index)) {
+            lowResItems.value.push(index);
+          }
+        }
+      }
+    });
+  },
+  { rootMargin: "100px 0px" }
+);
 
 const mappedImages = computed(() => {
   return images.map((image, ix) => {
@@ -66,15 +66,6 @@ const mappedImages = computed(() => {
 const onClick = (index: number) => {
   emit("update:selectItem", index);
 };
-
-onMounted(() => {
-  if (selectedItem) {
-    const element = imageRefs.value?.[selectedItem];
-    if (element) {
-      element.scrollIntoView({ behavior: "smooth", block: "center" });
-    }
-  }
-});
 </script>
 
 <template>
