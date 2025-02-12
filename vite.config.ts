048afd68abf0b15fb6ef0a0cfacf2e481c235335
Danny <danjmurphy92@gmail.com>
2025-02-12 17:37:25 +0000

    Fixing build

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import favicons from "@peterek/vite-plugin-favicons";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [vue(), favicons("./src/web-client/public/logo.jpg")],
+  plugins: [vue(), tailwindcss(), favicons("./src/web-client/public/logo.jpg")],
   publicDir: "./src/web-client/public",
   build: {
     target: "esnext",
