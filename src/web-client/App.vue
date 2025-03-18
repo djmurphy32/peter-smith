e8dc531682c6773d5cfc660dff6c1f7399e52fbf
Danny Murphy <danjmurphy92@gmail.com>
2025-03-18 07:41:36 +0000

    Upgrading to tailwind v4 (#293)
    * Upgrading to tailwind v4
    
    * adding padding

--- src/web-client/App.vue
+++ src/web-client/App.vue
@@ -8,14 +8,16 @@ import SpriteSheet from "./components/sprite/SpriteSheet.vue";
   <SpriteSheet />
   <main class="bg-neutral">
     <div
-      class="flex flex-col min-h-screen md:grid md:grid-cols-[200px,auto,200px] lg:grid-cols-[250px,auto,250px] xl:grid-cols-[300px,auto,300px]"
+      class="flex flex-col min-h-screen md:grid md:grid-cols-[200px_auto_200px] lg:grid-cols-[250px_auto_250px] xl:grid-cols-[300px_auto_300px]"
     >
       <div class="flex justify-center">
-        <div class="md:fixed md:top-1/2 md:transform md:-translate-y-1/2">
+        <div
+          class="py-4 md:py-0 md:fixed md:top-1/2 md:transform md:-translate-y-1/2"
+        >
           <Name />
         </div>
       </div>
-      <div class="flex-grow grid grid-rows-[1fr,auto] md:pt-16">
+      <div class="grow grid grid-rows-[1fr_auto] md:pt-16 px-4 md:px-0">
         <div class="flex justify-center items-end">
           <router-view />
         </div>
